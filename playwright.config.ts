854e7d89b846fee8bf3f06b9fcff9337905856f6
binsacedillo <beansgioacedillo@gmail.com>
2026-05-09 09:42:33 +0800

    fix: apply battle-tested localhost + ignoreHTTPSErrors setup [Audit Time: 2026-05-09 09:42:24]

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   timeout: 60 * 1000,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://localhost:3000',
     actionTimeout: 15 * 1000,
     navigationTimeout: 30 * 1000,
 
@@ -60,7 +60,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://127.0.0.1:3000',
+    url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 180 * 1000,
   },
