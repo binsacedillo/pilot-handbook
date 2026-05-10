e3204fcbbe051239919f48479b24e41ebdc4bbe6
binsacedillo <beansgioacedillo@gmail.com>
2026-05-10 09:34:16 +0800

    \fix: deploy Ghostbuster fix to purge Clerk JS from E2E [Audit Time: 2026-05-10 09:34:09]"

--- components/providers/ClerkProviderClient.tsx
+++ components/providers/ClerkProviderClient.tsx
@@ -6,5 +6,11 @@ import React from "react";
 type Props = { children: React.ReactNode };
 
 export default function ClerkProviderClient({ children }: Props) {
+  const isE2E = process.env.NEXT_PUBLIC_E2E === "true";
+
+  if (isE2E) {
+    return <>{children}</>;
+  }
+
   return <ClerkProvider>{children}</ClerkProvider>;
 }
